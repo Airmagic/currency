fadfbeb73ffea91bb2363741f6d878d2d3f35ab7
Airmagic <babaphat@yahoo.com>
2017-10-10 15:58:04 -0500

    week 7
    Uploading the week 7 inclass

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var exchangeRates = require('../model/currencyDB');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET convert page */
+ router.get('/convert', function(req, res, next){
+	 
+ 
+   var dollars = req.query.dollars;   // How many dollars?
+   var toCurrency = req.query.to_currency;  // To what currency?
+ 
+   var converted = dollars * exchangeRates[toCurrency];   // math!
+ 
+   res.render('results', {
+     dollars: dollars,
+     toCurrency: toCurrency,
+     converted: converted}
+	 );
+ });
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { name: 'Most Awsome Power, Mr. Sparkels', author: 'Eric H' });
+});
+
+module.exports = router;
